debd6040f99d4aa0085afd9b888b436809ec2a35
Importantus <importantus@mailbox.org>
2024-02-14 21:12:15 +0100

    :bug: Add workaround for vue components not being able to reference images in public folder

--- user/astro.config.mjs
+++ user/astro.config.mjs
@@ -6,7 +6,14 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()],
+  integrations: [tailwind(), vue({
+    // Workaround for https://github.com/withastro/astro/issues/9328
+    template: {
+      transformAssetUrls: {
+        includeAbsolute: false,
+      },
+    },
+  })],
   output: "server",
   adapter: node({
     mode: "standalone"
