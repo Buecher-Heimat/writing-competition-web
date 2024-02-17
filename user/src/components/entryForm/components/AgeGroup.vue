b29fd3d0b001f03c894cb49eded6a99fe67c9bcf
Importantus <importantus@mailbox.org>
2024-02-17 20:06:57 +0100

    :lipstick: Create an agegroup component

---
+++ user/src/components/entryForm/components/AgeGroup.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useEntryFormStore, pinia } from '../../../lib/entryFormStore';
+import { checkIfAgeIsInRange, createAgeGroupString } from '../../../lib/ageGroups';
+
+const props = defineProps<{
+    ageGroup: { age_start: number | null | undefined, age_end: number | null | undefined };
+}>();
+
+const store = useEntryFormStore(pinia);
+</script>
+
+<template>
+    <li class="border-[1px] text-sm border-bandicoot-400 rounded-lg px-2 py-1 text-bandicoot-400"
+        :class="{ 'bg-bandicoot-400 text-white': checkIfAgeIsInRange(store.formData.age_author, [props.ageGroup.age_start, props.ageGroup.age_end]) }">
+        {{ createAgeGroupString([props.ageGroup.age_start, props.ageGroup.age_end]) }} Jahre
+    </li>
+</template>
