8c1f8f14bb3868ac2ae1a217e31a51260d9e2684
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-02-24 13:44:50 +0100

    :construction: started with competitionform

---
+++ organizer/src/components/CompetitionForm.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import type { Competition } from '@/payload';
+
+const model = defineModel<Competition>()
+</script>
+<template>
+    <div>
+        <h1>{{ model?.title }}</h1>
+    </div>
+</template>
--- organizer/src/views/competition/EditView.vue
+++ organizer/src/views/competition/EditView.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import CompetitionForm from '@/components/CompetitionForm.vue';
+import { useCompetitionStore } from '@/stores/competition';
+
+const store = useCompetitionStore()
 
 </script>
+
 <template>
-    <div>
-        <h1>Edit Competition</h1>
-    </div>
+    <div class="px-10">
+        <h1 class="my-10 font-bold text-2xl font-serif small-caps text-twine-400">Wettbewerb bearbeiten</h1>
+        <div>
+            <CompetitionForm v-model="store.competition" />
+        </div>
+    </div> 
 </template>
