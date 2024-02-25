4e6643afff2bc0978808f83fe41b69566f8dede0
Tjalf <tjalf-bjarne.scharnweber@stud.th-luebeck.de>
2024-02-25 20:53:52 +0100

    :construction: build competitionform with bare logic

--- organizer/src/components/CompetitionForm.vue
+++ organizer/src/components/CompetitionForm.vue
@@ -1,10 +1,355 @@
 <script setup lang="ts">
-import type { Competition } from '@/payload';
+import { environment } from '@/lib/environment'
+import type { Competition, Media } from '@/payload'
+import { FileUp, PlusCircle } from 'lucide-vue-next'
+import InputWrapper from './InputWrapper.vue'
+import GroupedInputWrapper from './GroupedInputWrapper.vue'
+import { ref } from 'vue'
 
-const model = defineModel<Competition>()
+const model = defineModel<Competition>({ required: true, type: Object })
+
+const validation = [
+  {
+    message: () => 'Der Titel des Wettbewerbs ist ungültig.',
+    validate: () => {
+      return model.value.title.length > 0
+    }
+  },
+  {
+    message: () => 'Der Untertitel des Wettbewerbs ist ungültig.',
+    validate: () => {
+      return model.value.sponsor_string.length > 0
+    }
+  },
+  {
+    message: () => 'Das Startdatum des Wettbewerbs ist ungültig.',
+    validate: () => {
+      return model.value.date_start.length > 0
+    }
+  },
+  {
+    message: () => 'Das Enddatum des Wettbewerbs ist ungültig.',
+    validate: () => {
+      return model.value.date_end.length > 0
+    }
+  },
+  {
+    message: () => 'Es wird mindestens eine Altersgruppe benötigt.',
+    validate: () => {
+      return model.value.agegroups?.length > 0
+    }
+  },
+  {
+    message: () => 'Die Altersgruppe(n) ist/sind ungültig.',
+    validate: () => {
+      return model.value.agegroups?.some((agegroup) => {
+        return agegroup.age_start > 0
+      })
+    }
+  },
+  {
+    message: () => 'Es wird mindestens ein Sponsor benötigt.',
+    validate: () => {
+      return model.value.sponsors?.length > 0
+    }
+  },
+  {
+    message: () => 'Der Name eines oder mehreren Sponsoren ist ungültig.',
+    validate: () => {
+      return model.value.sponsors?.some((sponsor) => {
+        return sponsor.name.length > 0
+      })
+    }
+  },
+  {
+    message: () => 'Der Link eines oder mehreren Sponsoren ist ungültig.',
+    validate: () => {
+      return model.value.sponsors?.some((sponsor) => {
+        // TODO: Add a regex to check if the link is valid
+        return sponsor.link.length > 0
+      })
+    }
+  },
+  {
+    message: () => 'Das Logo eines oder mehreren Sponsoren ist ungültig.',
+    validate: () => {
+      return model.value.sponsors?.some((sponsor) => {
+        return sponsor.logo?.url.length > 0
+      })
+    }
+  },
+  {
+    message: () => 'Es wird mindestens ein Anleitungsschritt benötigt.',
+    validate: () => {
+      return model.value.instruction_steps?.length > 0
+    }
+  },
+  {
+    message: () => 'Der Titel eines oder mehreren Anleitungsschritten ist ungültig.',
+    validate: () => {
+      return model.value.instruction_steps?.some((step) => {
+        return step.title.length > 0
+      })
+    }
+  },
+  {
+    message: () => 'Die Beschreibung eines oder mehreren Anleitungsschritten ist ungültig.',
+    validate: () => {
+      return model.value.instruction_steps?.some((step) => {
+        return step.description.length > 0
+      })
+    }
+  }
+]
+
+const showHints = ref(false)
+const emit = defineEmits(['save'])
 </script>
 <template>
-    <div>
-        <h1>{{ model?.title }}</h1>
-    </div>
+  <div class="max-w-2xl w-full">
+    <form
+      @submit.prevent="
+        () => {
+          emit('save')
+        }
+      "
+      class="flex flex-col gap-8"
+    >
+      <div class="flex flex-col gap-4">
+        <h1 class="text-2xl font-bold small-caps">Allgemein</h1>
+        <div class="flex flex-row gap-8">
+          <img
+            :src="environment.backendUrl + (model?.image_hero as Media).url"
+            :alt="(model?.image_hero as Media).alt"
+            class="h-[15rem] w-[15rem] rounded-lg shadow-lg object-cover"
+          />
+          <div class="flex flex-col justify-between py-3">
+            <div>
+              <h2 class="font-bold small-caps mb-1">Titelbild</h2>
+              <p class="text-sm">
+                Das Titelbild wird an verschiedenen Stellen auf der Webseite genutzt, um eine
+                bessere Zielgruppenansprache zu ermöglichen und das Layout freundlicher zu
+                gestalten. Es sollte also die Zielgruppe des Wettbewerbs möglichst genau abgebildet
+                werden. Außerdem wird das Bild in verschiedenen Seitenverhältnissen angezeigt, an
+                den Rändern sollte also nichts Wichtiges zu sehen sein.
+              </p>
+            </div>
+            <button
+              class="w-fit flex gap-2 items-center text-pearl-bush-50 hover:text-twine-400 bg-twine-400 hover:bg-transparent border-twine-400 border rounded-lg px-3 py-2 shadow"
+            >
+              <FileUp class="w-5 h-5" />
+              Hochladen
+            </button>
+          </div>
+        </div>
+        <InputWrapper title="Titel *" description="Der Titel des Wettbewerbs">
+          <input
+            :value="model?.title"
+            type="text"
+            required
+            class="w-full bg-transparent border-bandicoot-400 focus:border-bandicoot-400 focus:ring-twine-400"
+          />
+        </InputWrapper>
+        <InputWrapper
+          title="Untertitle *"
+          description="Ein kurzer Satz, der den Wettbewerb genauer beschreibt und ggf. auf den/die Sponsor/en eingeht."
+        >
+          <input
+            :value="model?.sponsor_string"
+            type="text"
+            required
+            class="w-full bg-transparent border-bandicoot-400 focus:border-bandicoot-400 focus:ring-twine-400"
+          />
+        </InputWrapper>
+        <InputWrapper
+          title="Zeitraum *"
+          description="Start und Enddatum des Wettbewerbs. ACHTUNG: Wenn Du einen Wettbewerb planst, der in der Zukunft liegt, wird der auf der Webseite erst angezeigt, sobald der Einreichungszeitraum beginnt."
+        >
+          <div class="flex flex-row gap-4 items-center">
+            <p class="font-bold small-caps mb-1">Von</p>
+            <input
+              :value="model?.date_start"
+              type="date"
+              required
+              class="w-full bg-transparent border-bandicoot-400 focus:border-bandicoot-400 focus:ring-twine-400"
+            />
+            <p class="font-bold small-caps mb-1">Bis</p>
+            <input
+              :value="model?.date_end"
+              type="date"
+              required
+              class="w-full bg-transparent border-bandicoot-400 focus:border-bandicoot-400 focus:ring-twine-400"
+            />
+          </div>
+        </InputWrapper>
+      </div>
+      <div class="flex flex-col gap-4">
+        <div>
+          <h1 class="text-2xl font-bold small-caps mb-1">Sponsoren</h1>
+          <p class="text-sm">
+            Die Sponsoren des Wettbewerbs. Sie werden auf der Webseite und in der Ausschreibung
+            genannt.
+          </p>
+        </div>
+        <div v-for="(sponsor, index) in model?.sponsors">
+          <GroupedInputWrapper title="Sponsor" :index="index + 1">
+            <InputWrapper title="Name des Sponsors *">
+              <input
+                :value="sponsor?.name"
+                type="text"
+                required
+                class="w-full bg-transparent border-bandicoot-400 focus:border-bandicoot-400 focus:ring-twine-400"
+              />
+            </InputWrapper>
+            <InputWrapper title="Link zur Sponsoren Webseite *">
+              <input
+                :value="sponsor?.link"
+                type="text"
+                required
+                class="w-full bg-transparent border-bandicoot-400 focus:border-bandicoot-400 focus:ring-twine-400"
+              />
+            </InputWrapper>
+            <div class="flex flex-row gap-8 items-center">
+              <div class="w-3/4 flex justify-center">
+                <img
+                  :src="environment.backendUrl + (sponsor.logo as Media).url"
+                  :alt="environment.backendUrl + (sponsor.logo as Media).alt"
+                  class="h-[5rem] w-fit shadow-lg object-cover"
+                />
+              </div>
+              <div class="flex flex-col gap-4">
+                <div>
+                  <h2 class="font-bold small-caps mb-1">Titelbild</h2>
+                  <p class="text-sm">
+                    Das Logo des Sponsors sollte in einer möglichst nicht zu geringen Auflösung
+                    hochgeladen werden.
+                  </p>
+                </div>
+                <button
+                  class="w-fit flex gap-2 items-center text-pearl-bush-50 hover:text-twine-400 bg-twine-400 hover:bg-transparent border-twine-400 border rounded-lg px-3 py-2 shadow"
+                >
+                  <FileUp class="w-5 h-5" />
+                  Hochladen
+                </button>
+              </div>
+            </div>
+          </GroupedInputWrapper>
+        </div>
+        <button
+          class="w-fit flex gap-2 items-center text-bandicoot-400 hover:text-pearl-bush-50 hover:bg-bandicoot-400 border-bandicoot-400 border rounded-lg px-3 py-2 shadow"
+        >
+          <PlusCircle class="w-5 h-5" />
+          Sponsor hinzufügen
+        </button>
+      </div>
+      <div class="flex flex-col gap-4">
+        <div>
+          <h1 class="text-2xl font-bold small-caps mb-1">Altersgruppen</h1>
+          <p class="text-sm">Die Altersgruppen, die am Wettbewerb teilnehmen können.</p>
+        </div>
+        <div>
+          <div v-for="(ageGroup, index) in model?.agegroups" class="mb-4">
+            <GroupedInputWrapper title="Altersgruppe" :index="index + 1">
+              <div class="flex flex-col gap-4 mb-4">
+                <p class="text-sm">
+                  Altersspanne für die Teilnehmenden in Jahren von bis. Wird nur der erste Wert
+                  eingetragen, ist die Alterspanne nach oben offen. (z.B. 18-25 Jahre, 65+ Jahre)
+                </p>
+                <div class="flex flex-row gap-4 items-center">
+                  <p class="font-bold small-caps mb-1">Von</p>
+                  <input
+                    :value="ageGroup?.age_start"
+                    type="number"
+                    required
+                    class="w-full bg-transparent border-bandicoot-400 focus:border-bandicoot-400 focus:ring-twine-400"
+                  />
+                  <p class="font-bold small-caps mb-1">Bis</p>
+                  <input
+                    :value="ageGroup?.age_end"
+                    type="number"
+                    class="w-full bg-transparent border-bandicoot-400 focus:border-bandicoot-400 focus:ring-twine-400"
+                  />
+                </div>
+              </div>
+            </GroupedInputWrapper>
+          </div>
+        </div>
+        <button
+          class="w-fit flex gap-2 items-center text-bandicoot-400 hover:text-pearl-bush-50 hover:bg-bandicoot-400 border-bandicoot-400 border rounded-lg px-3 py-2 shadow"
+        >
+          <PlusCircle class="w-5 h-5" />
+          Altersgruppe hinzufügen
+        </button>
+      </div>
+      <div class="flex flex-col gap-4">
+        <div class="flex flex-col gap-4">
+          <div>
+            <h1 class="text-2xl font-bold small-caps mb-1">Anleitungsschritte</h1>
+            <p class="text-sm">
+              Schritte, die eine Nutzer bei der Teilnahme durchlaufen soll. Es soll Anleiten was
+              zutun ist. Zusehen auf der Wettbewerbseite (siehe Bild).
+            </p>
+          </div>
+          <img
+            src="../assets/guideStep_example.png"
+            alt="Beispielvorschau von zwei Anleitungsschritten"
+            class="w-full h-fit rounded-lg shadow-lg"
+          />
+          <p class="text-sm">
+            Die Anleitungsschritte werden auf der Wettbewerbsseite angezeigt und sollen die
+            Teilnehmer*innen durch den Prozess der Teilnahme leiten.
+          </p>
+        </div>
+        <div v-for="(step, index) in model?.instruction_steps">
+          <GroupedInputWrapper title="Schritt" :index="index + 1">
+            <InputWrapper title="Titel *">
+              <input
+                :value="step?.title"
+                type="text"
+                required
+                class="w-full bg-transparent border-bandicoot-400 focus:border-bandicoot-400 focus:ring-twine-400"
+              />
+            </InputWrapper>
+            <InputWrapper title="Beschreibung *">
+              <input
+                :value="step?.description"
+                type="text"
+                required
+                class="w-full bg-transparent border-bandicoot-400 focus:border-bandicoot-400 focus:ring-twine-400"
+              />
+            </InputWrapper>
+          </GroupedInputWrapper>
+        </div>
+        <button
+          class="w-fit flex gap-2 items-center text-bandicoot-400 hover:text-pearl-bush-50 hover:bg-bandicoot-400 border-bandicoot-400 border rounded-lg px-3 py-2 shadow"
+        >
+          <PlusCircle class="w-5 h-5" />
+          Sponsor hinzufügen
+        </button>
+      </div>
+      <div class="relative mt-5 mb-20">
+        <div class="absolute bottom-16 w-full z-20 flex flex-col gap-1" v-if="showHints">
+          <p
+            v-for="v in validation.filter((v) => !v.validate())"
+            class="rounded text-pearl-bush-50 bg-warning-600 p-3 font-semibold"
+          >
+            {{ v.message() }}
+          </p>
+        </div>
+        <button
+          type="submit"
+          class="w-full p-4 text-pearl-bush-50 font-bold rounded-lg"
+          :disabled="!validation.every((v) => v.validate())"
+          :class="{
+            'bg-twine-400': validation.every((v) => v.validate()),
+            'bg-twine-400/50 cursor-not-allowed': !validation.every((v) => v.validate())
+          }"
+          @mouseenter="showHints = true"
+          @mouseleave="showHints = false"
+        >
+          Speichern
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
---
+++ organizer/src/components/GroupedInputWrapper.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  title: string
+  index?: number
+}>()
+</script>
+
+<template>
+  <div class="w-full h-fit border border-bandicoot-400">
+    <div class="bg-pearl-bush-50 border-b border-bandicoot-400 py-2 px-4">
+      <h2 class="text-lg font-bold small-caps">{{ title + ' ' + (index ?? '') }}</h2>
+    </div>
+    <div class="p-4">
+      <slot></slot>
+    </div>
+  </div>
+</template>
