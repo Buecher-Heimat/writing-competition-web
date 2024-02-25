ef3b591a105dbec26eaa10d075a137c449ebbcaa
Importantus <Betterplayer01@gmail.com>
2024-02-25 13:11:19 +0100

    :art: Rename Milkdown component

--- organizer/src/components/MilkdownEditor.vue
+++ organizer/src/components/MilkdownEditor.vue
@@ -2,7 +2,7 @@
 import { Milkdown, useEditor } from '@milkdown/vue';
 import { defaultValueCtx, Editor, rootCtx, editorViewOptionsCtx, commandsCtx, editorViewCtx } from '@milkdown/core';
 import { listener, listenerCtx } from '@milkdown/plugin-listener';
-import { commonmark, toggleEmphasisCommand, toggleStrongCommand, toggleInlineCodeCommand, wrapInBlockquoteCommand, insertHrCommand } from '@milkdown/preset-commonmark'
+import { commonmark, toggleEmphasisCommand, toggleStrongCommand, toggleInlineCodeCommand, wrapInBlockquoteCommand } from '@milkdown/preset-commonmark'
 import { history, redoCommand, undoCommand } from '@milkdown/plugin-history';
 import { clipboard } from '@milkdown/plugin-clipboard';
 import { trailing } from '@milkdown/plugin-trailing';
@@ -30,7 +30,7 @@ const editor = useEditor((root) => {
       }))
       ctx.set(rootCtx, root)
       ctx.set(defaultValueCtx, content.value)
-      ctx.get(listenerCtx).markdownUpdated((ctx, markdown) => {
+      ctx.get(listenerCtx).markdownUpdated((_, markdown) => {
         content.value = markdown;
         contentLength.value = document.getElementById('milkdown')?.innerText.length || 0;
         emit('update:contentLength', contentLength.value);
