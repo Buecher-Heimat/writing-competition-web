a2d49013f2205fbf633474ea28f47886a7c23f1f
Importantus <importantus@mailbox.org>
2024-02-26 21:58:16 +0100

    :bug: Move agegroup selection into global form

--- user/src/components/entryForm/components/AgeGroup.vue
+++ user/src/components/entryForm/components/AgeGroup.vue
@@ -8,13 +8,6 @@ const props = defineProps<{
 }>();
 
 const store = useEntryFormStore(pinia);
-
-watch(() => store.formData.age_author, () => {
-    if (
-        checkIfAgeIsInRange(store.formData.age_author, [props.ageGroup.age_start, props.ageGroup.age_end])) {
-        store.formData.agegroup = props.ageGroup;
-    }
-});
 </script>
 
 <template>
@@ -22,7 +15,8 @@ watch(() => store.formData.age_author, () => {
         :class="{ 'bg-bandicoot-400 text-white': checkIfAgeIsInRange(store.formData.age_author, [props.ageGroup.age_start, props.ageGroup.age_end]) }">
         {{ createAgeGroupString([props.ageGroup.age_start, props.ageGroup.age_end]) }} Jahre
     </li> -->
-    <li v-if="checkIfAgeIsInRange(store.formData.age_author, [props.ageGroup.age_start, props.ageGroup.age_end])" class="border-[1px] text-sm border-bandicoot-400 rounded-lg px-2 py-1 bg-bandicoot-400 text-white">
+    <li v-if="checkIfAgeIsInRange(store.formData.age_author, [props.ageGroup.age_start, props.ageGroup.age_end])"
+        class="border-[1px] text-sm border-bandicoot-400 rounded-lg px-2 py-1 bg-bandicoot-400 text-white">
         {{ createAgeGroupString([props.ageGroup.age_start, props.ageGroup.age_end]) }} Jahre
     </li>
 </template>
